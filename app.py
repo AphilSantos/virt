a909308f98f9e41aab2044c2f003b5fb4b12a076
AaronS <aphilvs@gmail.com>
2023-10-26 14:06:37 +0800

    name and forms

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #create a flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key is 1"
 
+#create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #create a route decrator
 @app.route('/')
@@ -15,7 +23,6 @@ def index():
     favorite_pizza = ["Pepperoni", "Cheese"]
     return render_template("index.html", first_name=first_name, favorite_pizza=favorite_pizza)
 
-
 @app.route('/user/<name>')
 
 def user(name):
@@ -26,11 +33,26 @@ def user(name):
 #invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
-    render_template("404.html"), 404
+    return render_template("404.html"), 404
 
 #Internal error
 @app.errorhandler(500)
 def page_not_found(e):
-    render_template("500.html"), 500
+    return render_template("500.html"), 500
 
-    
+
+#create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template("name.html",
+                    name = name,
+                    form = form
+                    )
